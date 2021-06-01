016ddc7105bfd4384ee5fce3ce920742150de7d5
gusta <gustavo.barbosa060@gmail.com>
2021-06-01 11:16:42 -0300

    .

--- app/cliente.py
+++ app/cliente.py
@@ -7,9 +7,37 @@ from classes.Suporte import Suporte
 def home():
     return render_template('home.html')
 
+@app.route("/teste_gratis")
+def testegratis():
+    return render_template("teste_gratis.html")
+
+@app.route("/cases_sucesso")
+def casesdesucesso():
+    return render_template("cases_sucesso.html")
+
+@app.route("/duvidas_frequentes")
+def duvidasfrequentes():
+    return render_template("duvidas_frequentes.html")
+
 @app.route("/suporte")
 def suporte():
     return render_template("suporte.html")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/cadastraCli", methods=["GET", "POST"])
+def cadastrarCli():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        email = request.form["email_c"]
+        numero = request.form["numero"]
+        data_de_nascimento = request.form["data_de_nascimento"]
+        senha = request.form["senha"]
+        return render_template("home.html")
+    return "/cadastro"
+
 
 @app.route("/envia", methods=["GET", "POST"])
 def envia():
