c6f383d989889bc4d56d333154d20fa07f3a4fbb
Jackson Da Silva Menezes <76071751+jacksondasilvamenezes@users.noreply.github.com>
2021-06-01 21:22:00 -0300

    Add files via upload

---
+++ flask_auth_app/templates/main.py
@@ -0,0 +1,50 @@
+from flask import Blueprint,render_template
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route("/templates/")
+def index():
+    return render_template('index.html')
+
+
+
+@main.route("/templates/teste_gratis")
+def teste_gratis():
+    return render_template('teste_gratis.html')
+
+@main.route("/templates/signup")
+def signup():
+    return render_template('signup.html',signup)
+
+@main.route("/templates/cases_sucesso")
+def cases_sucesso():
+    return render_template('cases_sucesso.html',cases_sucesso)
+
+@main.route("/templates/cases_sucesso_2")
+def cases_sucesso_2():
+    return render_template('cases_sucesso_2.hmtl', cases_sucesso_2)
+
+@main.route("/templates/cases_sucesso_3")
+def cases_sucesso_3():
+    return render_template('cases_sucesso_3.html',cases_sucesso_3)
+
+@main.route("/templates/cases_sucesso_4")
+def cases_sucesso_4():
+    return render_template('cases_sucesso_4.html', cases_sucesso_4)
+
+@main.route("/templates/cases_sucesso_5")
+def cases_sucesso_5():
+    return render_template('cases_sucesso_5.html', cases_sucesso_5)
+
+@main.route("/templates/duvidas_frequentes")
+def duvidas_frequentes():
+    return render_template('Duvidas_frequentes.html', duvidas_frequentes)
+
+@main.route("/templates/Suporte")
+def suporte():
+    return render_template('Suporte.html', suporte)
+
+@main.route("/templates/cursos")
+def cursos():
+    return render_template('cursos.html', cursos)
