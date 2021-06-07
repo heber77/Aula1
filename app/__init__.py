c700b202130feb0600757ea209581b9359fb8afc
Jackson Da Silva Menezes <76071751+jacksondasilvamenezes@users.noreply.github.com>
2021-06-06 21:39:10 -0300

    Delete __init__.py

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_bcrypt import Bcrypt
-
-app = Flask(__name__, template_folder='templates')  # por default use o nome da pasta templates
-bcrypt = Bcrypt(app)
-
-from app import cliente
-from app import admin
-from app import main
